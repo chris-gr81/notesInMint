2c16ceabec8c6f92b44ece827414455dc31977fb
Christian Grimm <grimmchristian@gmx.de>
2025-03-18 22:19:11 +0100

    Refactor getCategoryObject and restyle btn 'neue notiz'

--- category.js
+++ category.js
@@ -5,35 +5,38 @@ let currentCategory = "";
 function renderNavbar() {
   navbarEl.innerHTML = "";
   const notes = getNotes();
-  const categoryList = getCategoryList(notes);
-  categoryList.forEach((category) => {
-    if (currentCategory == category.category) {
-      navbarEl.appendChild(createNavbarCategory(category, true));
-    } else {
-      navbarEl.appendChild(createNavbarCategory(category, false));
-    }
-  });
+  const categoryObject = getCategoryObject();
+
+  for (const category in categoryObject) {
+    console.log(category);
+    navbarEl.appendChild(
+      createNavbarCategory(
+        category,
+        categoryObject[category],
+        currentCategory === category
+      )
+    );
+  }
 }
 
-function getCategoryList() {
+function getCategoryObject() {
   const notes = getNotes();
-  const categoryList = [];
-  const categorySet = new Set();
-  notes.forEach((note) => {
-    categorySet.add(note.category);
+  const categoryObject = {
+    Alle: notes.length,
+    Unsortiert: countEntriesByCategory("Unsortiert"),
+  };
+  const notesForIteration = notes.filter((note) => {
+    return note.category !== "Unsortiert";
   });
 
-  categoryList.push({ category: "Alle", number: notes.length });
-  categoryList.push({
-    category: "Unsortiert",
-    number: countEntriesByCategory("Unsortiert"),
+  notesForIteration.forEach((note) => {
+    if (note.category in categoryObject) {
+      categoryObject[note.category]++;
+    } else {
+      categoryObject[note.category] = 1;
+    }
   });
-  categorySet.forEach((setItem) => {
-    if (setItem == "Unsortiert") return;
-    counter = countEntriesByCategory(setItem);
-    categoryList.push({ category: setItem, number: counter });
-  });
-  return categoryList;
+  return categoryObject;
 }
 
 function countEntriesByCategory(item) {
@@ -56,14 +59,14 @@ function filterCategory(notes) {
   }
 }
 
-function createNavbarCategory(category, isCat) {
+function createNavbarCategory(category, catNumber, isCat) {
   const categoryBtn = document.createElement("button");
   categoryBtn.classList.add("btn");
   categoryBtn.classList.add("category-btn");
   if (isCat) {
     categoryBtn.classList.add("selected-category");
   }
-  categoryBtn.setAttribute("data-category", category.category);
+  categoryBtn.setAttribute("data-category", category);
   categoryBtn.addEventListener("click", (e) => {
     clickOnCategory(e.currentTarget);
   });
@@ -79,9 +82,9 @@ function createNavbarCategory(category, isCat) {
                     d="M2 3.5A1.5 1.5 0 0 1 3.5 2h2.879a1.5 1.5 0 0 1 1.06.44l1.122 1.12A1.5 1.5 0 0 0 9.62 4H12.5A1.5 1.5 0 0 1 14 5.5v1.401a2.986 2.986 0 0 0-1.5-.401h-9c-.546 0-1.059.146-1.5.401V3.5ZM2 9.5v3A1.5 1.5 0 0 0 3.5 14h9a1.5 1.5 0 0 0 1.5-1.5v-3A1.5 1.5 0 0 0 12.5 8h-9A1.5 1.5 0 0 0 2 9.5Z"
                   />
                 </svg>
-                <span class="btn-text">${category.number}</span>
+                <span class="btn-text">${catNumber}</span>
               </div>
-              <span class="btn-text">${category.category}</span>      
+              <span class="btn-text">${category}</span>      
       `;
   return categoryBtn;
 }
